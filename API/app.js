9250d051fe288526be3f7ec0da72b6a03c20a6ae
jake-ward2639 <jake.ward2639@gmail.com>
2023-02-08 14:13:27 +0000

    API has login/signup functionality

--- API/app.js
+++ API/app.js
@@ -3,14 +3,102 @@ const express = require('express');
 const fileUpload = require('express-fileupload');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const db = require('./db');
 const fs = require("fs");
 const ejs = require("ejs");
-var server = http.createServer(function(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    var message = 'It works!\n',
-        version = 'NodeJS ' + process.versions.node + '\n',
-        response = [message, version].join('\n');
-    res.end(response);
-});
-server.listen();
+const db = require('./db');
+
+const app = express();
+app.use(bodyParser.urlencoded({extended: false}));
+
+const port = 3000;
+
+app.get('/jwTrainingAPI/login', async (req, res) => {
+    const {status, data} = await getUser(req);
+    res.status(status);
+    if(data) res.json(data);
+    else res.end();       
+})
+
+app.post('/jwTrainingAPI/signup', async (req, res) => {
+    const {status, data} = await postUser(req);
+    res.status(status);
+    if(data) res.json(data);
+    else res.end();
+})
+
+app.put('/jwTrainingAPI', async (req, res) => {
+    res.status(405);
+    res.end();
+})
+
+app.delete('/jwTrainingAPI', async (req, res) => {
+    res.status(405);
+    res.end();
+})
+
+async function getUser(req) {
+    let status = 500, data = null;
+    try {
+        const username = req.query.username;
+        const password = req.query.password;
+        if(username && password
+        && username.length > 0 && username.length <= 32
+        && username.match(/^[a-z0-9]+$/i)
+        && password.length > 0 && password.length <= 32){
+
+            const sql = 'SELECT `userId`, `email`, `username` FROM `users` WHERE `username`=? AND `password`=?';
+            const result = await db.query(sql, [username, password]);
+
+            if(result){
+                status = 200;
+                data = {
+                    'result': result
+                };
+            } else {
+                status = 204;
+            }
+            
+        } else {
+            status = 400;   
+        }
+    } catch(e) {
+        console.error(e);
+    }
+    return {status, data};
+}
+
+async function postUser(req) {
+    let status = 500, data = null;
+    try {
+        const email = req.body.email;
+        const username = req.body.username;
+        const password = req.body.password;
+        if(email && username && password
+        && email.match(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/)
+        && email.length > 0 && email.length <= 64 
+        && username.match(/^[a-z0-9]+$/i)
+        && username.length > 0 && username.length <= 64
+        && password.length > 0 && password.length <= 64){
+
+            const sql = 'INSERT INTO `users` (`email`, `username`, `password`) '
+            + 'VALUES (?, ?, ?)';
+            const result = await db.query(sql, [email, username, password]);
+
+            if(result.affectedRows) {
+                status = 201;
+                data = {'id': result.insertId };
+            }
+            
+        } else {
+            status = 400;
+        }
+    } catch(e) {
+        console.error(e);
+    }
+    return {status, data};
+}
+   
+
+app.listen(port, () => {
+ console.log(`running at http://localhost:${port}`)
+})
