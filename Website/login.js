b0eb7a2874fb812aa8686e285061b45c4fd87d7d
jake-ward2639 <jake.ward2639@gmail.com>
2023-02-13 17:44:00 +0000

    repeat password authentication + icon

--- Website/login.js
+++ Website/login.js
@@ -26,37 +26,46 @@ addEventListener('load', (event) => {
     }    
 
     submitSignup = () => {
-        let submitSignupHeaders = new Headers();
-        submitSignupHeaders.append("Content-Type", "application/x-www-form-urlencoded");
 
-        let urlencoded = new URLSearchParams();
-        urlencoded.append("email", document.querySelector('#signup-email').value);
-        urlencoded.append("username", document.querySelector('#signup-username').value);
-        urlencoded.append("password", document.querySelector('#signup-password').value);
+        if (document.querySelector('#signup-password').value == document.querySelector('#signup-password-repeat').value){
 
-        let requestOptions = {
-        method: 'POST',
-        headers: submitSignupHeaders,
-        body: urlencoded,
-        redirect: 'follow'
-        };
+            let submitSignupHeaders = new Headers();
+            submitSignupHeaders.append("Content-Type", "application/x-www-form-urlencoded");
 
-        fetch("https://jw1448.brighton.domains/jwTrainingAPI/signup", requestOptions)
-        .then(response => {
-            if (response.status == 200){
-                setCookie('username', document.querySelector('#signup-username').value, 1);
-                setCookie('password', document.querySelector('#signup-password').value, 1);
-                window.location.replace("https://jw1448.brighton.domains/jwTraining");
-            } else if (response.status == 400){
-                response.json().then(res => {
-                    document.querySelector('#signup-alert-content').textContent = res.error;
-                    document.querySelector('#signup-alert').style.display = "block";
-                }).catch(error);
-            }
-            return response.json();
-        })
-        .then(result => {})
-        .catch(error => {});
+            let urlencoded = new URLSearchParams();
+            urlencoded.append("email", document.querySelector('#signup-email').value);
+            urlencoded.append("username", document.querySelector('#signup-username').value);
+            urlencoded.append("password", document.querySelector('#signup-password').value);
+
+            let requestOptions = {
+            method: 'POST',
+            headers: submitSignupHeaders,
+            body: urlencoded,
+            redirect: 'follow'
+            };
+
+            fetch("https://jw1448.brighton.domains/jwTrainingAPI/signup", requestOptions)
+            .then(response => {
+                if (response.status == 200){
+                    setCookie('username', document.querySelector('#signup-username').value, 1);
+                    setCookie('password', document.querySelector('#signup-password').value, 1);
+                    window.location.replace("https://jw1448.brighton.domains/jwTraining");
+                } else if (response.status == 400){
+                    response.json().then(res => {
+                        document.querySelector('#signup-alert-content').textContent = res.error;
+                        document.querySelector('#signup-alert').style.display = "block";
+                    }).catch(error);
+                }
+                return response.json();
+            })
+            .then(result => {})
+            .catch(error => {});
+
+        } else {
+            document.querySelector('#signup-alert-content').textContent = 'Input passwords do not match';
+            document.querySelector('#signup-alert').style.display = "block";
+        }
+
     }
     
     function setCookie(cname, cvalue, exdays) {
