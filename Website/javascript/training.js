07a25687716dc5d148f0eecf6d52f21857dfb5cf
jake-ward2639 <jake.ward2639@gmail.com>
2023-03-01 13:56:34 +0000

    Split json array in half and display in columns

--- Website/javascript/training.js
+++ Website/javascript/training.js
@@ -2,15 +2,52 @@ addEventListener('load', (event) => {
     if ((sessionStorage.getItem('JWusername') && sessionStorage.getItem('JWpassword')) || 
     (document.cookie.includes('JWusername') && document.cookie.includes('JWpassword'))) {
         
-        var requestOptions = {
+        let requestOptions = {
             method: 'GET',
             redirect: 'follow'
         };
           
         fetch("https://jw1448.brighton.domains/jwTrainingAPI/training?username="+getCookie('JWusername')+"&password="+getCookie('JWpassword'), requestOptions)
             .then(response => response.json())
-            .then(result => console.log(result))
+            .then(result => {
+                let half = Math.ceil(result.result.length / 2);
+                let firstHalf = result.result.slice(0, half);
+                let secondHalf = result.result.slice(half);
+
+                for (let i = 0; i < firstHalf.length; i++) {
+                    let container = document.querySelector('#training_column1');
+                    let card = createCard(firstHalf[i].title, firstHalf[i].description);
+                    container.appendChild(card);
+                  }
+                  
+                  for (let i = 0; i < secondHalf.length; i++) {
+                    let container = document.querySelector('#training_column2');
+                    let card = createCard(secondHalf[i].title, secondHalf[i].description);
+                    container.appendChild(card);
+                  }
+            })
             .catch(error => console.log('error', error));
 
     }
+
+    function createCard(title, description) {
+        let card = document.createElement('div');
+        card.classList.add('card');
+      
+        let cardContainer = document.createElement('div');
+        cardContainer.classList.add('container');
+      
+        let cardTitle = document.createElement('h4');
+        cardTitle.textContent = title;
+      
+        let cardDescription = document.createElement('p');
+        cardDescription.textContent = description;
+      
+        cardContainer.appendChild(cardTitle);
+        cardContainer.appendChild(cardDescription);
+        card.appendChild(cardContainer);
+      
+        return card;
+    }
+
 })
