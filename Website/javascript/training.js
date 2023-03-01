196c16c335e196b51291682c693d85729030b4d3
jake-ward2639 <jake.ward2639@gmail.com>
2023-03-01 14:47:29 +0000

    links added to card

--- Website/javascript/training.js
+++ Website/javascript/training.js
@@ -16,37 +16,40 @@ addEventListener('load', (event) => {
 
                 for (let i = 0; i < firstHalf.length; i++) {
                     let container = document.querySelector('#training_column1');
-                    let card = createCard(firstHalf[i].title, firstHalf[i].description);
+                    let card = createCard(firstHalf[i].articleId, firstHalf[i].title, firstHalf[i].description);
                     container.appendChild(card);
-                  }
-                  
-                  for (let i = 0; i < secondHalf.length; i++) {
+                }
+                
+                for (let i = 0; i < secondHalf.length; i++) {
                     let container = document.querySelector('#training_column2');
-                    let card = createCard(secondHalf[i].title, secondHalf[i].description);
+                    let card = createCard(secondHalf[i].articleId, secondHalf[i].title, secondHalf[i].description);
                     container.appendChild(card);
-                  }
+                }
             })
             .catch(error => console.log('error', error));
 
     }
 
-    function createCard(title, description) {
+    function createCard(articleId, title, description) {
         let card = document.createElement('div');
         card.classList.add('card');
-      
+        
         let cardContainer = document.createElement('div');
         cardContainer.classList.add('container');
-      
+        
         let cardTitle = document.createElement('h4');
-        cardTitle.textContent = title;
-      
+        let titleLink = document.createElement('a');
+        titleLink.href = 'https://jw1448.brighton.domains/jwTrainingAPI/article?articleId='+articleId;
+        titleLink.textContent = title;
+        cardTitle.appendChild(titleLink);
+        
         let cardDescription = document.createElement('p');
         cardDescription.textContent = description;
-      
+        
         cardContainer.appendChild(cardTitle);
         cardContainer.appendChild(cardDescription);
         card.appendChild(cardContainer);
-      
+        
         return card;
     }
 
